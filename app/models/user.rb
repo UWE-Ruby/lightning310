e0cfc87edd5e97fb123d04e7f565f5f0d350b1b6
Renée Hendricksen <renee@nird.us>
2014-07-03 19:40:31 -0400

    working login w/ github

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,10 @@
 class User < ActiveRecord::Base
   devise :omniauthable, omniauth_providers: [:github]
+
+  def self.from_omniauth(auth)
+    where(auth.slice(:provider, :uid)).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+    end
+  end
 end
