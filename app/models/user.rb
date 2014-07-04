93bc11de22bbd4754e7abe7fda4810e96c396c71
Renée Hendricksen <renee@nird.us>
2014-07-03 20:11:58 -0400

    working login with github, have sign-in and sign-out buttons, capturing github info

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,12 @@
 class User < ActiveRecord::Base
-  devise :omniauthable, omniauth_providers: [:github]
+  devise :trackable, :omniauthable, omniauth_providers: [:github]
 
   def self.from_omniauth(auth)
     where(auth.slice(:provider, :uid)).first_or_create do |user|
       user.email = auth.info.email
-      user.password = Devise.friendly_token[0,20]
+      user.github_name = auth.info.nickname
+      user.name = auth.info.name
+      user.image = auth.info.image
     end
   end
 end
