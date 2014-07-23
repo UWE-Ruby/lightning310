47ca8c5049215ab1e5ab18c37e6ed10be6b48fc7
psytau <rmanninoFoo@gmail.com>
2014-07-23 19:22:49 +0800

    force tablets to display desktop view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   before_action :set_locale
+  before_filter :force_tablet_html
 
   has_mobile_fu
 
@@ -17,5 +18,9 @@ class ApplicationController < ActionController::Base
 
   rescue_from CanCan::AccessDenied do |exception|
     redirect_to :back, alert: exception.message
+  end
+
+  def force_tablet_html
+    session[:tablet_view] = false
   end
 end
