5ef19e61c17a14cc4da81398a3a5fe0f64a62670
Renée Hendricksen <renee@nird.us>
2014-08-07 17:25:23 -0700

    update the image when a user logs in if it's changed

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,11 +10,18 @@ class User < ActiveRecord::Base
   end
 
   def self.from_omniauth(auth)
-    where(auth.slice(:provider, :uid)).first_or_create do |user|
+    user = where(auth.slice(:provider, :uid)).first_or_create do |user|
       user.email = auth.info.email
       user.github_name = auth.info.nickname
       user.name = auth.info.name
       user.image = auth.info.image
     end
+    if user.image != auth.info.image
+      user.image = auth.info.image
+      user.save
+    end
+    user
+
   end
+
 end
